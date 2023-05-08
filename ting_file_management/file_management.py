2b1faf2193403d33fb07bbbf6767765b15e9e318
Ludson Pereira <ludson_ps27@hotmail.com>
2023-05-08 16:18:23 -0300

    feat: :sparkles: add função txt_importer
    função importa e valida qualquer arquivo no formato txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+import os
+
+
+def txt_importer(path_file: str) -> list[str]:
+    if not os.path.exists(path_file):
+        return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+    if os.path.splitext(path_file)[1].lower() != ".txt":
+        return print("Formato inválido", file=sys.stderr)
+
+    with open(path_file, 'r') as file:
+        content = file.read().split("\n")
+        return content
