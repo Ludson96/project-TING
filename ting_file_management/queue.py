82937060157dde9fbe8172e4b95150188e32082d
Ludson Pereira <ludson_ps27@hotmail.com>
2023-05-08 11:23:19 -0300

    feat: :sparkles: add classe TAD fila Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,20 +3,22 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self._data = list()
+        self._items = list()
 
-    def __len__(self):
-        return len(self._data)
+    def __len__(self) -> int:
+        return len(self._items)
 
-    def enqueue(self, value):
-        self._data.append(value)
+    def enqueue(self, value: int) -> None:
+        if not isinstance(value, int):
+            raise TypeError("O valor deve ser um inteiro.")
+        self._items.append(value)
 
-    def dequeue(self):
-        if len(self._data) == 0:
+    def dequeue(self) -> None | int:
+        if len(self._items) == 0:
             return None
-        return self._data.pop(0)
+        return self._items.pop(0)
 
-    def search(self, index):
-        if (index < 0) or (index > len(self._data)):
-            # raise IndexError
-        return self._data[index]
+    def search(self, index: int) -> Exception | int:
+        if (index < 0) or (index > (len(self._items) - 1)):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self._items[index]
