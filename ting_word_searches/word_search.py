491aab3972a5f50b52fa43f50a4f73552759ffc7
Ludson Pereira <ludson_ps27@hotmail.com>
2023-05-09 18:15:23 -0300

    feat: :sparkles: add função exist_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    if not word:
+        return None
+
+    result = []
+    for item in instance._items:
+        ocorrencias = [
+            {"linha": linha + 1}
+            for linha, conteudo in enumerate(item["linhas_do_arquivo"])
+            if word.lower() in conteudo.lower()
+        ]
+
+        if ocorrencias:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": item["nome_do_arquivo"],
+                    "ocorrencias": ocorrencias,
+                }
+            )
+
+    return result
 
 
 def search_by_word(word, instance):
