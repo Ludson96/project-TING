9a1107fbe95d861f12f34973368052261d06a789
Ludson Pereira <ludson_ps27@hotmail.com>
2023-05-09 16:56:22 -0300

    feat: :sparkles: add teste para a classe priority_queue

---
+++ tests/priority_queue/mock_data.py
@@ -0,0 +1,11 @@
+alta_prioridade = {
+    "nome_do_arquivo": "arquivo_teste.txt",
+    "qtd_linhas": 3,
+    "linhas_do_arquivo": ["linhas"],
+}
+
+baixa_prioridade = {
+    "nome_do_arquivo": "arquivo_teste2.txt",
+    "qtd_linhas": 10,
+    "linhas_do_arquivo": ["Linhas"],
+}
--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,26 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+from tests.priority_queue.mock_data import alta_prioridade, baixa_prioridade
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority = PriorityQueue()
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        priority.search(3)
+
+    priority.enqueue(baixa_prioridade)
+    assert len(priority.regular_priority) == 1
+
+    priority.enqueue(alta_prioridade)
+    assert len(priority.high_priority) == 1
+    assert len(priority) == 2
+
+    assert priority.search(0) == alta_prioridade
+    assert priority.search(1) == baixa_prioridade
+
+    item_removido = priority.dequeue()
+    assert item_removido == alta_prioridade
+    assert len(priority.regular_priority) == 1
+    assert len(priority.high_priority) == 0
+    assert len(priority) == 1
